2a2cd064907a762b221791486ea4312611ad3878
Christian Mohindo <christian.mohindo@epitech.eu>
2020-03-27 22:20:05 +0100

    add allow orign

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.use(express.static(__dirname + '/public'));
 server.listen(8080);
 // WARNING: app.listen(80) will NOT work here!
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  socket.emit('news', { hello: 'FROM THE SERVER' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
