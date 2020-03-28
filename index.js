ea90b0497b3c17a7bca575a2bdc60514d37a705f
Christian Mohindo <christian.mohindo@epitech.eu>
2020-03-28 13:17:02 +0100

    add design

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost"); // update to match the domain you will make the request from
@@ -9,7 +9,7 @@ app.use(function(req, res, next) {
     next();
   });
 
-var args = process.argv.slice(2);
+const args = process.argv.slice(2);
     app.use(express.static(__dirname + '/public'));
     server.listen(8080);
 
@@ -17,20 +17,36 @@ var height, width, imgUrl;
 
 console.log(args);
 //GET ARGS FROM SERVER: npm start -- <height> <width> <imgUrl>
-if(args.length() >= 3){
+if(args.length >= 3){
     height = args[0];
     width = args[1];
     imgUrl = args[2];
+}else{
+    console.error("Missing parementers: npm start -- <height> <width> <imgUrl>");
 }
-// WARNING: app.listen(80) will NOT work here!
+//render index.html
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'FROM THE SERVER' });
-  socket.on('my other event', function (data) {
+    socket.emit('client_connect', { hello: 'FROM THE SERVER' });
+    socket.on('my other event', function (data) {
     console.log(data);
   });
+});
+//WHen player disconnect from channel
+io.on('disconnect', function(socket){
+
+});
+
+//When playing is writting
+io.on('message', function(from, msg){
+
+});
+
+// When an user is playing
+io.on('playing', function(data){
+    socket.broadcast.emit('playing', {socket: socket.username});
 });
